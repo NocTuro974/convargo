3a1f39e8958bf64a3b22a0d5fea854e35798e435
NocTuro974 <31305710+NocTuro974@users.noreply.github.com>
2018-01-20 14:11:30 +0100

    Ajout de la partie déductible & Paiement des acteurs

--- public/index.js
+++ public/index.js
@@ -146,68 +146,134 @@ const actors = [{
 }];
 
 
-function calculatePrice()
+function calculatePrice(d)
 {
-  for(var d = 0; d < deliveries.length; d++)
+  var ppk;
+  var ppv;
+  var calculatedPrice;
+  var commission;
+  var partInsurance;
+  var partTreasury;
+  var partConvargo;
+  var fraisOption = 0;
+  var facture;
+
+  for(var t = 0; t < truckers.length; t++)
   {
-    var ppk;
-    var ppv;
-    var calculatedPrice;
-    var commission;
-    var partInsurance;
-    var partTreasury;
-    var partConvargo;
-
-    for(var t = 0; t < truckers.length; t++)
+    if(truckers[t].id == deliveries[d].truckerId)
     {
-      if(truckers[t].id == deliveries[d].truckerId)
-      {
-        ppk = truckers[t].pricePerKm;
-        ppv = truckers[t].pricePerVolume;
+      ppk = truckers[t].pricePerKm;
+      ppv = truckers[t].pricePerVolume;
 
-        if (deliveries[d].volume > 5 && deliveries[d].volume <= 10)
+      if (deliveries[d].volume > 5 && deliveries[d].volume <= 10)
+      {
+        ppv = 0.90 * ppv;
+      }
+      if (deliveries[d].volume > 10 && deliveries[d].volume <= 25)
+      {
+        ppv = 0.70 * ppv;
+      }
+      if (deliveries[d].volume > 25)
+      {
+        ppv = 0.50 * ppv;
+      }
+      if (deliveries[d].options.deductibleReduction == true)
+      {
+        ppv = ppv + 1
+      }
+    }
+  }
+  calculatedPrice = ppk * deliveries[d].distance + ppv * deliveries[d].volume;
+  deliveries[d].price = calculatedPrice;
+  console.log("calculated price : " + calculatedPrice);
+
+  if (deliveries[d].options.deductibleReduction == true)
+  {
+    fraisOption = deliveries[d].volume
+    commission = 0.3 * (calculatedPrice - fraisOption)
+  }
+  else
+  {
+    commission = 0.3 * calculatedPrice      
+  }
+  console.log("commission (calculated without option) is : " + commission)
+
+  partInsurance = 0.5 * commission
+  console.log("the insurance part is " + partInsurance)
+  deliveries[d].commission.insurance = partInsurance
+
+  if (deliveries[d].distance < 500)
+  {
+    partTreasury = 1
+    deliveries[d].commission.treasury = partTreasury
+  }
+  else
+  {
+    var reste = deliveries[d].distance % 500
+    partTreasury = (deliveries[d].distance - reste) / 500
+    deliveries[d].commission.treasury = partTreasury;
+  }
+  console.log("the treasury part is " + partTreasury)
+
+  partConvargo = commission + fraisOption - partTreasury - partInsurance
+  deliveries[d].commission.convargo = partConvargo;
+  console.log("the convargo part is " + partConvargo)
+  console.log("")
+
+  facture = {
+    'id' : deliveries[d].id,
+    'calculatedPrice' : calculatedPrice,
+    'partInsurance' : partInsurance,
+    'partTreasury' : partTreasury,
+    'partConvargo' : partConvargo};
+
+    return facture;
+}
+
+function payActors(delivId, calculatedPrice, partInsurance, partTreasury
+  , partConvargo){
+
+  var amount;
+  for (var a = 0; a < actors.length; a++)
+  {
+    if (actors[a].deliveryId == delivId)
+    {
+      for (var p = 0; p < actors[a].payment.length; p++)
+      {
+        if (actors[a].payment[p].who == 'shipper')
         {
-          ppv = 0.90 * ppv;
+          actors[a].payment[p].amount = calculatedPrice;
+          console.log('shipper has been debited ' + calculatedPrice);
         }
-        if (deliveries[d].volume > 10 && deliveries[d].volume <= 25)
+        if (actors[a].payment[p].who == 'trucker')
         {
-          ppv = 0.70 * ppv;
+          amount = calculatedPrice - partInsurance - partTreasury - partConvargo;
+          actors[a].payment[p].amount = amount;
+          console.log('trucker has been paid ' + amount);
         }
-        if (deliveries[d].volume > 25)
+        if (actors[a].payment[p].who == 'treasury')
         {
-          ppv = 0.50 * ppv;
+          actors[a].payment[p].amount = partTreasury;
+          console.log('treasury has been paid ' + partTreasury);
+        }
+        if (actors[a].payment[p].who == 'insurance')
+        {
+          actors[a].payment[p].amount = partInsurance;
+          console.log("insurance has been paid " + partInsurance);
+        }
+        if (actors[a].payment[p].who == 'convargo')
+        {
+          actors[a].payment[p].amount = partConvargo;
+          console.log("convargo has been paid " + partConvargo);
         }
       }
     }
-    calculatedPrice = ppk * deliveries[d].distance + ppv * deliveries[d].volume;
-    deliveries[d].price = calculatedPrice;
-    console.log("calculated price : " + calculatedPrice);
-
-    commission = 0.3 * calculatedPrice
-    console.log("commission (30%) is : " + commission)
-
-    partInsurance = 0.5 * commission
-    console.log("the insurance part is " + partInsurance)
-    deliveries[d].commission.insurance = partInsurance
-
-    if (deliveries[d].distance < 500)
-    {
-      partTreasury = 1
-      deliveries[d].commission.treasury = partTreasury
-    }
-    else
-    {
-      var reste = deliveries[d].distance % 500
-      partTreasury = (deliveries[d].distance - reste) / 500
-      deliveries[d].commission.treasury = partTreasury;
-    }
-    console.log("the treasury part is " + partTreasury)
-
-    partConvargo = commission - partTreasury - partInsurance
-    deliveries[d].commission.convargo = partConvargo;
-    console.log("the convargo part is " + partConvargo)
-    console.log("")
   }
 }
 
-calculatePrice()
+for (var d = 0; d < deliveries.length; d++)
+{
+  var facture = calculatePrice(d);
+  payActors(facture.id, facture.calculatedPrice, facture.partInsurance, facture.partTreasury, facture.partConvargo);
+  console.log("");
+}
